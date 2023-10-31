a6a072033c705103681e8767e627247eda2aaee2
purshottam <pankajpatidar9521@gmail.com>
2023-10-31 13:14:50 +0530

    commit

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,45 @@
+package com.driver.model;
+
+import javax.persistence.*;
+@Entity
+@Table
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userName;
+    private String password;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Admin(){}
+    public Admin(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
